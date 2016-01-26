49ca0bf899eb84111684080c0ee575cd474b8fdd
Jr Reyes <jr.evilboss@gmail.com>
2016-01-26 12:00:31 +0800

    Checking out revisions to make sure I have all the files

--- app/lib/methods.js
+++ app/lib/methods.js
@@ -2,17 +2,4 @@
 /* Client and Server Methods */
 /*****************************************************************************/
 Meteor.methods({
-  /*
-   * Example:
-   *
-   * '/app/items/insert': function (item) {
-   *  if (this.isSimulation) {
-   *    // do some client stuff while waiting for
-   *    // result from server.
-   *    return;
-   *  }
-   *
-   *  // server method logic
-   * }
-   */
 });
