096da1f76f7685d3a9f329ac4ca4c52722659b3a
Jr Reyes <jr.evilboss@gmail.com>
2016-01-28 21:38:04 +0800

    completed merge refactor to 2 lines

--- app/lib/router/routes.js
+++ app/lib/router/routes.js
@@ -1,21 +1,18 @@
 Router.route('/', {
-    name: 'home',
-    controller: 'PublicController',
-    where: 'client',
-    action:'action'
+  name: 'home',
+  controller: 'PublicController',
+  where: 'client',
+  action: 'action'
 });
 
 Router.route('/dashboard/sales', {
-    name: 'dashboard.sales',
-    where: 'client',
-    controller:'DashboardController',
-    action:'sales'
+  name: 'dashboard.sales',
+  where: 'client',
+  controller: 'DashboardController',
+  action: 'sales'
 });
 
-
-
-
 Router.plugin('ensureSignedIn', {
-    except: ['home','atSignIn']
+  except: ['home', 'atSignIn']
 });
 Router.plugin('dataNotFound', {dataNotFoundTemplate: 'notFound'});
