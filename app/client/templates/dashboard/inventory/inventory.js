7595c6c4820869e1f1b779f89543a2d549cdcfe5
Jr Reyes <jr.evilboss@gmail.com>
2016-02-16 14:52:45 +0800

    Added Search to Inventory

--- app/client/templates/dashboard/inventory/inventory.js
+++ app/client/templates/dashboard/inventory/inventory.js
@@ -1,14 +1,28 @@
 /*****************************************************************************/
 /* Inventory: Event Handlers */
 /*****************************************************************************/
+let searchText = new ReactiveVar('');
 Template.Inventory.events({
-
+  'keyup #search-box': function (e) {
+    var text = $(e.target).val().trim();
+    searchText.set(text);
+  }
 });
 
 /*****************************************************************************/
 /* Inventory: Helpers */
 /*****************************************************************************/
 Template.Inventory.helpers({
+  InventoryList: function () {
+    let toSearch = searchText.get();
+    if (toSearch) {
+      return Inventory.find({
+        name: {$regex: '.*' + toSearch + '.*'}
+      });
+    } else {
+      return Inventory.find({});
+    }
+  }
 
 });
 
