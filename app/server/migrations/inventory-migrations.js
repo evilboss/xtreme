558ddc882c60edfa7b8fd42fb5abc3cf2dc74a1a
Jr Reyes <jr.evilboss@gmail.com>
2016-02-17 11:56:31 +0800

    Renamed inventory migrations

--- app/server/migrations/inventory-migrations.js
+++ app/server/migrations/inventory-migrations.js
@@ -2,7 +2,7 @@
  * Created by gilbertor on 2/3/16.
  */
 if (Inventory.find().count() === 0) {
-  Inventory.insert({name: 'Shampoo', quantity: 100, description: 'tide', price: 50});
+  Inventory.insert({name: 'Shampoo', quantity: 100, description: 'Shampoo', price: 50});
   Inventory.insert({name: 'Baby Powder', quantity: 100, description: 'JohnSons', price: 30});
-  Inventory.insert({name: 'Powder', quantity: 100, description: 'Surf', price: 20});
+  Inventory.insert({name: 'Powder', quantity: 100, description: 'Shampoo', price: 20});
 }
